8a46757b54d93fcd557a17aad42b2e01aa39d727
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-02-27 11:10:15 +0700

    named params

--- router_test.go
+++ router_test.go
@@ -13,16 +13,17 @@ import (
 
 func TestParams(t *testing.T) {
 	router := httprouter.New()
-	router.GET("/product/:id", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	router.GET("/product/:id/genre/:genrename", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		id := params.ByName("id")
-		text := "product " + id
+		genre := params.ByName("genrename")
+		text := "product " + id + " genre " + genre
 		fmt.Fprint(w, text)
 	})
-	r := httptest.NewRequest("GET", "htpp://localhost:8085/product/1", nil)
+	r := httptest.NewRequest("GET", "htpp://localhost:8085/product/1/genre/metal", nil)
 	recorder := httptest.NewRecorder()
 	router.ServeHTTP(recorder, r)
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
-	assert.Equal(t, "product 1", string(body))
+	assert.Equal(t, "product 1 genre metal", string(body))
 
 }
